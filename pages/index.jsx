568ab8b15b572bfe306222b6efaf141dc4c4ee36
Angela Caldas <aes.caldas@gmail.com>
2022-11-07 16:25:59 -0300

    feat: add Timeline styled components and banner

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,53 +1,60 @@
 import config from '../config.json';
 import styled from 'styled-components';
 
+import { CSSReset } from '../src/components/CSSReset';
+import Menu from '../src/components/Menu';
+import { StyledTimeline } from '../src/components/Timeline';
+
 function HomePage() {
   const mensagem = "Bem vindo ao AluraTube";
-  const style = { backgroundColor: "red" };
   return (
-    <div style={style}>
-      <Menu />
-      <Header />
-      <Timeline playlists={config.playlists}>
-        Conteúdo
-      </Timeline>
-    </div>
-  );
-}
-
-function Menu() {
-  return (
-    <div>Menu</div>
+    <>
+      <CSSReset />
+      <div>
+        <Menu />
+        <Header />
+        <Timeline playlists={config.playlists}>
+          Conteúdo
+        </Timeline>
+      </div>
+    </>
   );
 }
 
 const StyledHeader = styled.header`
-  img {
-    width: 80px;
-    height: 80px;
-    border-radius: 50%;
+  .banner {
+    width: 100%;
+    height: auto;
+    min-height: 200px;
+    max-height: 30vw;
+    object-fit: cover;
+    margin-top: 50px;
   }
   .user-info {
     display: flex;
     align-items: center;
     width: 100%;
     padding: 16px 32px;
-    gap: 16px
+    gap: 16px;
+    & .user-avatar {
+      width: 80px;
+      height: 80px;
+      border-radius: 50%;
+    }
   }
 `;
 
 function Header() {
   return (
     <StyledHeader>
-      <img src="banner" alt="" />
+      <img className='banner' src="/banner.png" alt="" aria-hidden='true' />
       <section className='user-info'>
-        <img src={`https://github.com/${config.github}.png`} alt="Avatar Angela Caldas" />
+        <img class="user-avatar" src={`https://github.com/${config.github}.png`} alt="Avatar Angela Caldas" />
         <div>
           <h2>{config.name}</h2>
           <p>{config.job}</p>
         </div>
       </section>
-      Cargo
     </StyledHeader>
   );
 }
@@ -55,16 +62,16 @@ function Header() {
 function Timeline(props) {
   const playlistNames = Object.keys(props.playlists);
   return (
-    <section>
+    <StyledTimeline>
       {playlistNames.map((playlistName) => {
         const videos = props.playlists[playlistName];
         return (
-          <section>
+          <section key={playlistName}>
             <h2>{playlistName}</h2>
             <div>
               {videos.map(video => {
                 return (
-                  <a href={video.url}>
+                  <a href={video.url} key={video.title}>
                     <img src={video.thumbnail} />
                     <span>
                       {video.title}
@@ -75,7 +82,7 @@ function Timeline(props) {
           </section>
         );
       })}
-    </section>
+    </StyledTimeline>
   );
 }
 
